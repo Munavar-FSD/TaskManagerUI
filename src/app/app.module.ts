daa0ad77d6ac3de017afe74a2d4460d72b1669bb
Munavar-FSD <58175081+Munavar-FSD@users.noreply.github.com>
2019-12-01 02:31:37 -0800

    FSD - Added new file changes for UI

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddtaskComponent } from './addtask/addtask.component';
+import { UpdatetaskComponent } from './updatetask/updatetask.component';
+import { ViewtaskComponent } from './viewtask/viewtask.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddtaskComponent,
+    UpdatetaskComponent,
+    ViewtaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
